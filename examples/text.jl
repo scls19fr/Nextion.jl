0b08078a39261e0b27552976b9b0d9c0a794dd8c
scls19fr <s.celles@gmail.com>
2018-08-17 17:09:48 +0200

    Alignment test

--- examples/text.jl
+++ examples/text.jl
@@ -2,14 +2,20 @@ using Nextion
 
 nexSerial = NexSerial("/dev/ttyUSB0")
 
-#nexText = NexText(nexSerial, Name("t0"), pid=PageID(0), cid=ComponentID(1))
-#nexText = NexText(nexSerial, Name("t0"))
-nexText = NexText(nexSerial, Name("t0"), cid=ComponentID(1))
-send(nexSerial, "page 0")
+#nexText = NexText(nexSerial, Name("t1"), pid=PageID(2), cid=ComponentID(1))
+#nexText = NexText(nexSerial, Name("t1"))
+nexText = NexText(nexSerial, Name("t1"), cid=ComponentID(1))
+
+reset(nexSerial)
+
+sleep(1)
+
+send(nexSerial, "page pg_text")
+
+sleep(1)
 
 setText(nexText, "Hello!")
 
-#=
 sleep(0.5)
 
 setText(nexText, "1")
@@ -35,9 +41,8 @@ sleep(0.5)
 setVisible(nexText, true)
 
 sleep(0.5)
-=#
 
-setAlignment(nexText, Alignment.Horizontal.LEFT)  # RIGHT CENTRE LEFT
+setAlignment(nexText, Alignment.Horizontal.RIGHT)  # RIGHT CENTRE LEFT
 setAlignment(nexText, Alignment.Vertical.DOWN)  # UP CENTRE DOWN
 
 #reset(nexSerial)
