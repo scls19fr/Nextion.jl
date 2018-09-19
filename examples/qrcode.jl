a305f757101a484d7611fc3cf3490599c358615b
scls19fr <scls19fr@users.noreply.github.com>
2018-09-19 21:38:07 +0200

    Update qrcode.jl

--- examples/qrcode.jl
+++ examples/qrcode.jl
@@ -1,24 +1,31 @@
 using Nextion
 
-nexSerial = NexSerial("/dev/ttyUSB0")
 
-nexQRcode = NexQRcode(nexSerial, Name("qr0"), cid=ComponentID(1))
-nexText = NexText(nexSerial, Name("t1"), cid=ComponentID(3))
+@testset "qrcode" begin
+    nexSerial = NexSerial("/dev/ttyUSB0")
 
-send(nexSerial, "page pg_qr")
+    nexQRcode = NexQRcode(nexSerial, Name("qr0"), cid=ComponentID(1))
+    nexText = NexText(nexSerial, Name("t1"), cid=ComponentID(3))
 
-sleep(1)
+    send(nexSerial, "page pg_qr")
 
-text = "Hello"
-nexQRcode.text = text
-nexText.text = text
+    sleep(1)
 
-sleep(2)
+    msg = "Hello"
+    nexQRcode.text = msg
+    nexText.text = msg
+  
+    @test nexQRcode.text = msg
 
-#text = "https://github.com/scls19fr/Nextion.jl"
-text = "http://bit.ly/2vWS75r"
-nexText.text = text
-#nexQRcode.textmaxlength = length(text)
-#nexQRcode.textmaxlength = 50
-#sleep(1)
-nexQRcode.text = text
+    sleep(2)
+
+    # text = "https://github.com/scls19fr/Nextion.jl"
+    msg = "http://bit.ly/2vWS75r"
+    nexText.text = msg
+    #nexQRcode.textmaxlength = length(text)
+    #nexQRcode.textmaxlength = 50
+    #sleep(1)
+    nexQRcode.text = msg
+
+    close(nexSerial)
+end
